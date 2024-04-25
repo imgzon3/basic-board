0f48c710f5430b6828968ccd31719e4d0771c7f1
imgzon3 <imgzon3@naver.com>
2024-04-25 21:44:39 +0900

    Feat: ErrorCode, ErrorResponse 추가 - 예외 응답 형식 통일 - 예외 응답 메시지는 ErrorCode enum에서 관리

---
+++ src/main/java/com/test/board/dto/error/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.test.board.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    //Common
+    INTERNAL_SERVER_ERROR(500, "C000", "Internal server error."),
+    INVALID_INPUT_VALUE(400, "C001", "Invalid input value."),
+    REQUEST_PARAMETER_MISSING(400, "C002", "Request Parameter is missing."),
+    HTTP_MESSAGE_NOT_READABLE(400, "C003", "HTTP request body is invalid."),
+    METHOD_NOT_ALLOWED(405, "C004", "HTTP method is not allowed")
+
+    //Post
+
+    //Member
+
+    ;
+
+    private int status;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/test/board/dto/error/ErrorResponse.java
@@ -0,0 +1,56 @@
+package com.test.board.dto.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private String code;
+    private List<FieldError> fieldErrors;
+
+
+    private ErrorResponse(final ErrorCode code, final List<FieldError> fieldErrors) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+        this.fieldErrors = fieldErrors;
+    }
+
+    private ErrorResponse(final ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse of(final ErrorCode code, final List<FieldError> fieldErrors) {
+        return new ErrorResponse(code, fieldErrors);
+    }
+
+    public static ErrorResponse of(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add(new FieldError(field, value, reason));
+            return fieldErrors;
+        }
+
+    }
+
+}
